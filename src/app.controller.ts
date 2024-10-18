dc9fe1fc52dee9340ddb28c1623b5496ed7d20c6
Mateo Vicentin <mateov@bluetrailsoft.com>
2024-10-18 17:27:09 -0300

    using global interceptor

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,9 +1,7 @@
-import { Controller, Get, UseInterceptors } from '@nestjs/common';
+import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
-import { LoggingInterceptor } from './logging-interceptor/logging-interceptor.service';
 
 @Controller()
-@UseInterceptors(LoggingInterceptor)
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,10 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { LoggingInterceptor } from './logging-interceptor/logging-interceptor.service';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.useGlobalInterceptors(new LoggingInterceptor());
   await app.listen(process.env.PORT ?? 3000);
 }
 void bootstrap();
